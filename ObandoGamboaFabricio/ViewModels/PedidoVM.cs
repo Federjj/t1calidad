068c64799239bc0d850011ef50a1ca3e88d731c2
kev-developer <kevpersonal3@.com>
2025-09-22 14:38:13 -0500

    fin

---
+++ ObandoGamboaFabricio/ViewModels/PedidoVM.cs
@@ -0,0 +1,13 @@
+﻿using ObandoGamboaFabricio.Models;
+
+namespace ObandoGamboaFabricio.ViewModels
+{
+    public class PedidoVM
+    {
+        public Pedido NuevoPedido { get; set; }
+        public List<DetallePedido> Detalles { get; set; }
+        public List<Cliente> Clientes { get; set; }
+        public List<Articulo> Articulos { get; set; }
+        public List<Pedido> Pedidos { get; set; }
+    }
+}
